fb0447442705fde2545080d720dfdf51685961a9
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-05 21:11:30 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class temp2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int start = 0;
+        int end = size-1;
+        
+        while(start < end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+        
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
