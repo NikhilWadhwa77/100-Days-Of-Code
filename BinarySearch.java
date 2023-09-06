9ab2f913d35a56a085d8cc7789d7a59ab49679b8
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-06 20:01:07 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int key = sc.nextInt();
+
+        int start = 0;
+        int end = n-1;
+        int mid;
+
+        while(start <= end){
+            mid = (start+end)/2;
+
+            if(arr[mid] == key){
+                System.out.println(mid);
+                return;
+            }
+            if(arr[mid] > key){
+                end = mid-1;
+            }
+            if(arr[mid] < key){
+                start = mid+1;
+            }
+        }
+        System.out.println(-1);
+    }
+}
