404f2971791134fdfbec6b8f2ec157d5cd1c30c1
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-08 21:56:06 +0530

    Update InterestingAlphabets.java

--- InterestingAlphabets.java
+++ InterestingAlphabets.java
@@ -1,1 +1,25 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
 
+public class Solution {
+
+	public static ArrayList<ArrayList<Character>> interestingPattern(int N) {
+
+		ArrayList<ArrayList<Character>> ls = new ArrayList<>();
+		for(int i = 0; i < N; i++){
+			ls.add(new ArrayList<>());
+		}
+
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j <= i; j++){
+				char result = (char)(64+N-i+j);
+				ls.get(i).add(result);
+				
+			}
+			System.out.println();
+		}
+
+		return ls;
+	}
+}
