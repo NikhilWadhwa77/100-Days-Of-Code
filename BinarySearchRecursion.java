a86b36ae212a939905eb2150315b6e9b97df94ec
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-09 22:05:51 +0530

    Update BinarySearchRecursion.java

--- BinarySearchRecursion.java
+++ BinarySearchRecursion.java
@@ -1,1 +1,30 @@
+package recursion;
 
+public class searchBinary {
+
+    static int binary(int[] arr, int key,int s, int e){
+
+        int mid = (s+e)/2;
+
+        if(s > e){
+            return -1;
+        }
+        if(arr[mid] == key){
+            return mid;
+        }
+        if(arr[mid] > key){
+            return binary(arr,key,s,mid-1);
+        }
+        if(arr[mid] < key){
+            return binary(arr,key,mid+1,e);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12,16,22,99,123,444,981};
+
+        System.out.println(binary(arr,13,0,arr.length-1));
+
+    }
+}
