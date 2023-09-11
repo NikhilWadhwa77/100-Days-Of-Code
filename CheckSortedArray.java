cddd238d53cd18185b9d06b0c398185082e60386
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-11 20:57:49 +0530

    Update CheckSortedArray.java

--- CheckSortedArray.java
+++ CheckSortedArray.java
@@ -1,1 +1,17 @@
+class IsSorted {
+  
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,2};
+        System.out.println(sorted(arr,0));
+    }
 
+    static boolean sorted(int[] arr, int i){
+        if(i == arr.length - 1){
+            return true;
+        }
+        if(arr[i] <= arr[i+1]){
+            return sorted(arr, ++i);
+        }
+        return false;
+    }
+}
