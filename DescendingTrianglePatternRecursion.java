2d9183a50fb99b97da7802bf074523edee0b9602
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-13 05:53:50 +0530

    Update DescendingTrianglePatternRecursion.java

--- DescendingTrianglePatternRecursion.java
+++ DescendingTrianglePatternRecursion.java
@@ -1,1 +1,31 @@
+/*
 
+n = 4
+
+****
+***
+**
+*
+
+ */
+
+public class DescendingTriangle {
+
+    static void print(int n, int i){
+        if(n == 0){
+            return;
+        }
+        if(i == n){
+            System.out.println();
+            print(n-1,0);
+        }else{
+            System.out.print("*");
+            print(n,i+1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        print(4,0);
+    }
+}
