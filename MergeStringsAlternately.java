8d8cc2b7bc91cfde9629a4b84b250d9afd71fac0
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-16 20:40:43 +0530

    Update MergeStringsAlternately.java

--- MergeStringsAlternately.java
+++ MergeStringsAlternately.java
@@ -1,1 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String merged = "";
+        
+        int i = 0;
 
+        while(i < word1.length() || i < word2.length()){
+            if(i < word1.length()){
+                merged += word1.charAt(i);
+            }
+            if(i < word2.length()){
+                merged += word2.charAt(i);
+            }
+            i++;
+        }
+       
+        return merged;
+    }
+}
