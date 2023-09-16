622aea4ed9cd3637c43aaae768dec3e0455dc1ef
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-16 20:59:55 +0530

    Update ReverseWordsString.java

--- ReverseWordsString.java
+++ ReverseWordsString.java
@@ -1,1 +1,16 @@
+class Solution {
+    public String reverseWords(String s) {
+        String str = s.trim();
+        String[] arr = str.split(" ");
 
+        String result = "";
+
+        for(int i = arr.length-1; i >= 0; i--){
+            if(arr[i] == ""){
+                continue;
+            }
+            result += arr[i] + " ";
+        }
+        return result.trim();
+    }
+}
