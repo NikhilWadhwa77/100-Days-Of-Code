57dee6d14b7a6799653f9bf1ee976fee3dddf969
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-18 21:35:57 +0530

    Update CanPlaceFlowers.java

--- CanPlaceFlowers.java
+++ CanPlaceFlowers.java
@@ -1,1 +1,33 @@
-
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(flowerbed.length == 1){
+            if(n == 1){
+                if(flowerbed[0] == 0) return true;
+                return false;
+            }
+            return true;
+        }
+        for(int i = 0; i < flowerbed.length; i++){
+            if(n == 0) return true;
+            if(i == 0){
+                if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n == 0) return true;
+                }
+            }
+            else if(i == flowerbed.length-1){
+                if(flowerbed[i-1] == 0 && flowerbed[i] == 0){
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n == 0) return true;
+                }
+            }else if(flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0){
+                flowerbed[i] = 1;
+                n--;
+                if(n == 0) return true;
+            }
+        }
+        return n == 0;
+    }
+}
