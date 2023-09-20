750222324508d66e4a4c95394b95d25ecd6e17d9
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-20 20:27:13 +0530

    Update LinkedListAddElement.java

--- LinkedListAddElement.java
+++ LinkedListAddElement.java
@@ -1,1 +1,48 @@
+package DataStructures;
 
+class LinkedList {
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    void add(int val){
+        Node newNode = new Node(val);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("NULL");
+    }
+
+}
+
+class Demo{
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        ll.print();
+    }
+}
