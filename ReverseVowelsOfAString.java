0519bd7098dede20aa6c060d5c227e30736b79c1
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-21 19:58:12 +0530

    Update ReverseVowelsOfAString.java

--- ReverseVowelsOfAString.java
+++ ReverseVowelsOfAString.java
@@ -1,1 +1,38 @@
+import java.util.*;
+class Solution {
 
+    static boolean isVowel(char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' ||
+        ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            return true;
+        }
+        return false;
+    }
+
+    public String reverseVowels(String s) {
+
+        ArrayList<Character> list = new ArrayList<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(isVowel(s.charAt(i))){
+                list.add(s.charAt(i));
+            }
+        }
+
+        Collections.reverse(list);
+        int j = 0;
+        String result = "";
+
+        for(int i = 0; i < s.length(); i++){
+            if(isVowel(s.charAt(i))){
+                result += list.get(j);
+                j++;
+            }
+            else{
+                result += s.charAt(i);
+            }
+        }
+
+        return result;
+    }
+}
