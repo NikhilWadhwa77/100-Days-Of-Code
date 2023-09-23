1c1bfad5bf7bc894f2bde375326d26394f2819bd
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-09-23 20:57:05 +0530

    Update MoveZeroes.java

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,1 +1,16 @@
-
+import java.util.Arrays;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] arr = new int[nums.length];
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                arr[j] = nums[i];
+                j++;
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = arr[i];
+        }
+    }
+}
